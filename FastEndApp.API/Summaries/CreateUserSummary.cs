0bc8b19353013b511a87f50f46d20f1d8046f923
abdumalik <mister.ak98@list.ru>
2024-10-11 09:18:59 +0500

    feat: create user summary

---
+++ FastEndApp.API/Summaries/CreateUserSummary.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper.Internal;
+using FastEndApp.API.Contracts.Requests;
+using FastEndApp.API.Contracts.Responses;
+using FastEndApp.API.Endpoints.Users;
+using FastEndpoints;
+
+namespace FastEndApp.API.Summaries;
+
+public class CreateUserSummary : Summary<CreateUserEndpoint>
+{
+    public CreateUserSummary()
+    {
+        Summary = "short summary goes here";
+        Description = "long description goes here";
+        ExampleRequest = new UserRequest { FirstName = "Ali", LastName = "Hakimov", Age = 37 };
+        Response<UserResponse>(200, "ok response with body", example: new() { Id = Guid.NewGuid(), FullName = "Karim Rasulov", Age = 56 });
+        Response<ErrorResponse>(400, "validation failure");
+        Response(404, "not found");
+    }
+}
